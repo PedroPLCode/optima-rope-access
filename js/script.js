f00f8d3808c2cd0a3caba5e9ae7ded3c5c6f0713
Piotr Gaszczynski <piotrek.gaszczynski@gmail.com>
2023-12-15 00:37:55 +0100

    Add scss styles for footer and navigation and js script AddEventListeners

--- js/script.js
+++ js/script.js
@@ -1,55 +1,97 @@
 const settings = {
   selectors: {
+    navigation: '.navigation',
+    footer: '.footer',
     contactButton: '.contact__btn',
+    contactButtonText: '.contact__btn-text',
     contactBox: '.contact__box',
+    closeButton: '.close__trigger',
     imagesBox: '.images',
-    image: 'img',
+    images: 'img',
   },
   classes: {
     boxActive: 'box__active',
     buttonActive: 'button__active',
     imageActive: 'image__active',
+    navigationHide: 'navigation__hide',
+    footerShow: 'footer__show',
   }
 }
 
-const contactButton = document.querySelector(settings.selectors.contactButton);
-const contactBox = document.querySelector(settings.selectors.contactBox);
-
 const imagesBox = document.querySelector(settings.selectors.imagesBox);
-const images = document.querySelectorAll(settings.selectors.image);
+const images = document.querySelectorAll(settings.selectors.images);
 
+// CONTACT SHOW AND HIDE - OK
+const contactButton = document.querySelector(settings.selectors.contactButton);
+const contactButtonText = document.querySelector(settings.selectors.contactButtonText);
+const contactBox = document.querySelector(settings.selectors.contactBox);
+const closeButton = document.querySelector(settings.selectors.closeButton);
 document.addEventListener( 'click', event => {
-  if (event.target == contactButton) {
+  if (event.target == contactButton || event.target == contactButtonText) {
     event.preventDefault();
     contactButton.classList.toggle(settings.classes.buttonActive);
     contactBox.classList.toggle(settings.classes.boxActive);
-  } /* else if (event.target != contactBox && event.target != contactBox.children) {
+  } else if (event.target == closeButton) {
     contactButton.classList.remove(settings.classes.buttonActive);
     contactBox.classList.remove(settings.classes.boxActive);
-  } */
-  for (let singleImage of images) {
+  }
+
+
+  // IMAGES - TO DO !!!
+  for (let image of images) {
     const imagesBoxRect = imagesBox.getBoundingClientRect();
-    if (event.target == singleImage) {
+    if (event.target == image) {
       //event.preventDefault();
-      if (!singleImage.classList.contains(settings.classes.imageActive)) {
-        singleImage.style.height = `${window.innerHeight / 1.5}px`;
-        singleImage.style.width = `auto`;
-        singleImage.style.top = `${window.top}`
-        singleImage.style.transform = `translateX(-${imagesBoxRect.width}px)`;
-        singleImage.classList.add(settings.classes.imageActive);
+      if (!image.classList.contains(settings.classes.imageActive)) {
+        image.style.height = `${window.innerHeight / 1.5}px`;
+        image.style.width = `auto`;
+        image.style.top = `${window.top}`
+        image.style.transform = `translateX(-${imagesBoxRect.width}px)`;
+        image.classList.add(settings.classes.imageActive);
       } else {
-        singleImage.style.height = '100%';
-        singleImage.style.width = `100%`;
-        singleImage.style.transform = ``;
-        singleImage.classList.remove(settings.classes.imageActive);
+        image.style.height = '100%';
+        image.style.width = `100%`;
+        image.style.transform = ``;
+        image.classList.remove(settings.classes.imageActive);
       }
 
-    } else if (event.target != singleImage) {
+    } else if (event.target != image) {
       //event.preventDefault();
-      singleImage.classList.remove(settings.classes.imageActive);
+      image.classList.remove(settings.classes.imageActive);
     }
   }
 });
 
+// NAVIGATION AND FOOTER HIDE - OK
+const navigation = document.querySelector(settings.selectors.navigation);
+const footer = document.querySelector(settings.selectors.footer);
+let previousScrollPosition = 0;
+let currentScrollPosition = 0;
+  window.addEventListener('scroll', event => {
+    event.preventDefault();
 
+    contactButton.classList.remove(settings.classes.buttonActive);
+    contactBox.classList.remove(settings.classes.boxActive);
 
+    for (let image of images) {
+      if (image.classList.contains(settings.classes.imageActive)) {
+        image.style.height = '100%';
+        image.style.width = `100%`;
+        image.style.transform = ``;
+        image.classList.remove(settings.classes.imageActive);
+      }
+    }
+
+    currentScrollPosition = window.pageYOffset;
+    if (previousScrollPosition - currentScrollPosition < 0) {
+      navigation.classList.add(settings.classes.navigationHide);
+    } else {
+      navigation.classList.remove(settings.classes.navigationHide);
+    }
+    previousScrollPosition = currentScrollPosition;
+    if (window.pageYOffset < 25) {
+      footer.classList.remove(settings.classes.footerShow);
+    } else {
+      footer.classList.add(settings.classes.footerShow);
+    }
+  });
